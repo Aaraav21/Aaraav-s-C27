334fdd3ca430096cbecedbbf812803b249858afd
Aaraav21 <59116980+Aaraav21@users.noreply.github.com>
2020-01-14 19:47:38 +0530

    Add files via upload

---
+++ Chain.js
@@ -0,0 +1,21 @@
+class chain{
+    constructor(bodyA,bodyB){
+var options=  {
+    bodyA:bodyA,
+    bodyB:bodyB,
+    stiffness:0.04,
+    length:6
+}
+this.chain=Constraint.create(options);
+World.add(world,this.chain);
+    }
+display(){
+    push();
+    var pointA=this.chain.bodyA.position;
+    var pointB=this.chain.bodyB.position;
+    strokeWeight(5);
+    fill("black");
+    line(pointA.x,pointA.y,pointB.x,pointB.y);
+    pop();
+}
+}
